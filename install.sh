f78426313ed297c578a468f1aa53141a681e293e
xiaoz <xiaoz93@outlook.com>
2024-01-31 20:24:10 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -6,20 +6,22 @@ RESTIC_VERSION="0.16.3"
 install_soft(){
     apt-get update
     # 安装mydumper和定时任务
-    apt-get install -y mydumper cron ca-certificates
+    apt-get install -y mydumper cron ca-certificates wget
 
-}
-
-# 安装restic
-install_restic(){
     wget https://soft.xiaoz.org/linux/restic_${RESTIC_VERSION}_linux_amd64
     mv restic_${RESTIC_VERSION}_linux_amd64 /usr/bin/restic
     chmod +x /usr/bin/restic
     chmod +x *.sh
+    # 创建数据目录
+    mkdir -p ./data
 }
+
+
 
 # 清理
 clean(){
+    # 移除wget
+    apt-get remove -y wget
     rm -rf /var/lib/apt/lists/*
     rm -rf /var/cache/apt/archives/*.deb
     rm -rf /var/cache/apt/archives/partial/*.deb
